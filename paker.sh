8c811584bea1a36a936e00667e6977f769ddb2b1
mw@localhost.me <mw@localhost.me>
2018-12-28 15:29:36 +0100

    comment script

--- paker.sh
+++ paker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Simple script for creating archives ready for installation in wordpress
+# this will list all NORMAL directories and compress them into files named like directories.
+# Entries like ., .., .git, .idea will be omitted.
+# Piotr Be <herbalist@herbalist.hekko24.pl>
+
 for dname in $(ls -d */ | tr -d /); do
     archive_name=$dname'.zip'
     zip -r $archive_name $dname
